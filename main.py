869c3b29858e0a6b6b0b42fa38cfcdce86dfb5b1
pierremvogo <mvogopierre129@gmail.com>
2024-06-30 11:51:11 +0100

    new commit

--- main.py
+++ main.py
@@ -6,8 +6,10 @@ from api.student import Student
 
 def main():
    student =  Student().getStudents()
+   student1 = Student().getStudentById(1)  #ICI
    loop = asyncio.get_event_loop()
    loop.run_until_complete(student)
+   loop.run_until_complete(student1)  #et ICI
    loop.close()
 
 if __name__ == "__main__":
